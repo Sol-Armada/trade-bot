59b3e5cbe1f49a165bf6e18e671673964d6ac976
Douglas DeVicariis <douglas@devicariis.org>
2022-07-20 14:15:02 -0400

    Last updates to updateTask Added check to see if the release is a draft Aded a release info command

--- tasks/updateTask.py
+++ tasks/updateTask.py
@@ -1,14 +1,20 @@
+import json
 import os
+from platform import release
 import shutil
 import subprocess
 import tarfile
+import discord
 import requests
 from datetime import datetime
 from os.path import exists
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
 
+from main import TradeBot
+
 load_dotenv()
+
 
 class UpdateTask(commands.Cog):
     download_loc = None
@@ -22,24 +28,26 @@ class UpdateTask(commands.Cog):
             }
             if not exists("./current_release.txt"):
                 open("./current_release.txt", 'a').close()
+            
             self.check.start()
 
     @tasks.loop(seconds=90)
     async def check(self):
         try:
             resp = self.client.get(f"https://api.github.com/repos/{os.getenv('GITHUB_REPO', 'sol-armada/discord-bot')}/releases")
-
+            
             rate_limit = int(resp.headers.get("X-RateLimit-Remaining"))
             if rate_limit > 0 and resp.status_code == 200:
                     latest_release = resp.json()[0]
                     download_link = latest_release["tarball_url"]
-                    if latest_release["id"] != self.get_current_release():
-                        # download the latest release
+
+                    if latest_release["id"] != self.get_current_release() and not bool(latest_release["draft"]):
                         resp = self.client.get(download_link)
                         with open(os.path.join(self.download_loc, "bot.tar.gz"), 'wb') as f:
                             f.write(resp.content)
                         self.save_current_release(latest_release["id"])
-                        self.apply_release()
+                        #self.apply_release()
+                        self.save_release_info(latest_release)
             else:
                 reset_time = datetime.fromtimestamp(int(resp.headers.get("X-RateLimit-Reset")))
                 time_left = reset_time - datetime.now()
@@ -50,6 +58,30 @@ class UpdateTask(commands.Cog):
     @check.error
     async def check_error(seld, error):
         print(error)
+
+    @commands.slash_command()
+    async def version(self, ctx: discord.ApplicationContext):
+        release_info = self.get_release_info()
+        embed_version = discord.Embed(
+            type="rich",
+        )
+        embed_version.add_field(name="Version", value=release_info["name"])
+        embed_version.add_field(name="Link", value=release_info["link"])
+
+        await ctx.respond(embed=embed_version)
+
+    def get_release_info(self) -> dict:
+        f = open("./release_info.txt", "r")
+        return json.loads(f.read())
+
+    def save_release_info(self, release_info):
+        with open("./release_info.txt", "w") as f:
+            f.write(json.dumps({
+                "link": release_info["html_url"],
+                "name": release_info["name"],
+                "prelease": bool(release_info["prerelease"]),
+                "published_at": release_info["published_at"]
+            }))
 
     def get_current_release(self) -> str:
         f = open("./current_release.txt", "r")
